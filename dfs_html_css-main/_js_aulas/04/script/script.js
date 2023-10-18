e4c949234d2ab9ee06ee43ccdbfec59a1cfba530
ALINY OLIVEIRA DA SILVA <aliny.osilva1@senacsp.edu.br>
2023-10-18 20:49:43 -0300

    a

---
+++ dfs_html_css-main/_js_aulas/04/script/script.js
@@ -0,0 +1,157 @@
+
+// let nota1 = parseFloat(prompt("nota01"))
+// let nota2 = parseFloat(prompt("nota02"))
+// let nota3 = parseFloat(prompt("nota03"))
+
+// let media = (nota1 + nota2 + nota3) /3
+
+// if(media < 5){
+//     console.log(`Aluno(a) reprovado(a) com média ${media}`)
+// } else if (media >= 5 && media < 7) {
+//     console.log(`Aluno(a) em recuperação com média ${media}`)
+// } else {
+//     console.log(`Aluno(a) aprovado com média ${media}`)
+// }
+
+// let numero = parseInt(prompt("Digite um número de 1 até 12"))
+
+// if(numero === 1){
+//     console.log("O mês digitado foi: janeiro")
+// }
+//  else if (numero === 2){
+//     console.log("O mês digitado foi: fevereiro")
+// }
+//  else if (numero === 3){
+//     console.log("O mês digitado foi: março")
+// }
+//  else if (numero === 4){
+//     console.log("O mês digitado foi: abril")
+// }
+//  else if (numero === 5){
+//     console.log("O mês digitado foi: maio")
+// }
+//  else if (numero === 6){
+//     console.log("O mês digitado foi: junho")
+// }
+//  else if (numero === 7){
+//     console.log("O mês digitado foi: julho")
+// }
+//  else if (numero === 8){
+//     console.log("O mês digitado foi: agosto")
+// }
+//  else if (numero === 9){
+//     console.log("O mês digitado foi: setembro")
+// }
+//  else if (numero === 10){
+//     console.log("O mês digitado foi: outubro")
+// }
+//  else if (numero === 11){
+//     console.log("O mês digitado foi: novembro")
+// }
+//  else if (numero === 12){
+//     console.log("O mês digitado foi: dezembro")
+// } else{
+//     console.log("Número inválido")
+// // }
+
+// let numero = parseInt(prompt("Digite um número de 1 até 12"))
+
+// switch (numero) {
+//     case 1:
+//         console.log("jan")
+//         break;
+//     case 2:
+//         console.log("fev")
+//         break;
+//     case 3:
+//         console.log("mar")
+//         break;
+//     case 4:
+//         console.log("abr")
+//         break;
+//     case 5:
+//         console.log("mai")
+//         break;
+//     case 6:
+//         console.log("jun")
+//         break;
+//     case 7:
+//         console.log("jul")
+//         break;
+//     case 8:
+//         console.log("ago")
+//         break;
+//     case 9:
+//         console.log("set")
+//         break;
+//     case 10:
+//         console.log("out")
+//         break;
+//     case 11:
+//         console.log("nov")
+//         break;
+//     case 12:
+//         console.log("dez")
+//         break;
+//     default:
+//         console.log("Mês inválido")
+// }
+
+
+// DESAFIO: 
+// let profissao = prompt("Qual é a sua profissão:")
+
+// switch(profissao){
+//     case "programador":
+//         console.log("Você é um Programador")
+//         break;
+//     case "advogado":
+//         console.log("Você é um Advogado")
+//         break;
+//     case "engenheiro":
+//         console.log("Você é um Engenheiro")
+//         break;
+//     default:
+//         console.log("Profissão não encontrada")
+// }
+
+
+// let contador = 1;
+
+// while(contador <= 5){
+//     console.log(contador)
+//     // contador++;
+//     contador = contador + 1
+// }
+
+
+
+// DESAFIO: Utilizando a estrutura while, solicite ao usuário que escolha um número de 0 a 10 e apresente a partir do número escolhido a somatória de 5 em 5 até 100 no console.
+
+// let contador = parseInt(prompt("Escolha um número e 0 a 100"))
+
+// while(contador <= 100){
+//     console.log(contador)
+//     contador = contador + 5
+// }
+
+
+// DESAFIO: Utilizando a estrutura while, solicite ao usuário que escolha um número de 0 a 10 e apresente a partir do número escolhido a subtração de 5 em 5 até 0 no console.
+
+let contador = parseInt(prompt("Escolha um número e 0 a 100"))
+
+while(contador >= 0){
+    console.log(contador)
+    contador -= 5
+}
+
+
+
+
+
+
+
+
+
+
+
